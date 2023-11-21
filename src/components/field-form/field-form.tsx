3459123bc47fe7cc92606737ff7f39f0ae05f6ab
LadaKuzmina <92179198+LadaKuzmina@users.noreply.github.com>
2023-11-21 17:47:45 +0500

    Добро пожаловать, или посторонним вход воспрещён (часть 2)

--- src/components/field-form/field-form.tsx
+++ src/components/field-form/field-form.tsx
@@ -6,13 +6,11 @@ type FieldFormProps = {
   className: string;
   innerRef: MutableRefObject<HTMLInputElement | null>;
 }
-const FieldForm = forwardRef(function FieldForm({placeholder, nameField, className, innerRef}: FieldFormProps) {
-    return (
-        <div className={className}>
-            <input className="sign-in__input" type={nameField} placeholder={placeholder} name={`user-${nameField}`} id={`user-${nameField}`} ref={innerRef}/>
-            <label className="sign-in__label visually-hidden" htmlFor={`user-${nameField}`}>{placeholder}</label>
-        </div>
-    );
-});
+const FieldForm = forwardRef(({placeholder, nameField, className, innerRef}: FieldFormProps) => (
+  <div className={className}>
+    <input className="sign-in__input" type={nameField} placeholder={placeholder} name={`user-${nameField}`} id={`user-${nameField}`} ref={innerRef}/>
+    <label className="sign-in__label visually-hidden" htmlFor={`user-${nameField}`}>{placeholder}</label>
+  </div>
+));
 
 export default FieldForm;
